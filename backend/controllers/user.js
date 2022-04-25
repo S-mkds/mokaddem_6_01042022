cfb68997ce46d51e5902a578acac7f0c10cec81f
John Doe <Mokaddem-s@hotmail.com>
2022-04-25 10:49:21 +0200

    Fin

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -31,17 +31,14 @@ exports.login = (req, res, next) => {
   })
     .then((user) => {
       if (!user) {
-        // if user not found, return an error 401 (unauthorized)
         return res.status(401).json({ error: "Utilisateur incorrecte" });
       }
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            // if password is not valid, return an error 401 (unauthorized)
             return res.status(401).json({ error: "mdp incorrecte" });
           }
-          // if password is valid, return a token
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
